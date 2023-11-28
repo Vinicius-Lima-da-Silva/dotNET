15b7a28833b21c15e08d8cfaacab24cea1043c76
Vinicius <vini_lima98@outlook.com>
2023-11-28 15:59:26 -0300

    classes

--- avaliacao/Program.cs
+++ avaliacao/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/ avaliacao 28-11
+
+using System;
+using System.Collections.Generic;
+
+public class Pessoa
+{
+    public string Nome { get; set; }
+    public DateTime DataNascimento { get; set; }
+    public string CPF { get; set; }
+}
+
+public class Treinador : Pessoa
+{
+    public string CREF { get; set; }
+}
+
+public class Cliente : Pessoa
+{
+    public double Altura { get; set; }
+    public double Peso { get; set; }
+}
+
+public class Academia
+{
+    private List<Treinador> treinadores;
+    private List<Cliente> clientes;
+
+    public Academia()
+    {
+        treinadores = new List<Treinador>();
+        clientes = new List<Cliente>();
+    }
